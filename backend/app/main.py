70aa05b394414c21e32066d73a931a0c2a170803
Eduardo <duduoli11@hotmail.com>
2024-06-14 20:43:54 -0300

    (feat): Added inital backend setup

---
+++ backend/app/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+
+from api.main import router as api_router
+
+app = FastAPI(title="rocketman-challenge")
+
+app.include_router(api_router, prefix="/api")
+
+@app.get("/", include_in_schema=False)
+async def ping() -> JSONResponse:
+    return JSONResponse({"ping": "pong"})
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
