9200aa5b6e5e54da63e095d9eeaff074787ced8a
Eduardo <duduoli11@hotmail.com>
2024-06-15 01:05:11 -0300

    (feat): Added route version

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,4 +1,4 @@
 from fastapi import APIRouter
 from .v1.pokemons.views import router as pokemons_router
-router = APIRouter()
+router = APIRouter(prefix='/v1')
 router.include_router(pokemons_router)
